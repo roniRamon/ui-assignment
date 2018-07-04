195b66b9a3b9664af120d594771a9b7cf9899909
RoniRamon <roniramon260@gmail.com>
2018-07-04 10:20:45 -0700

    fetch data from the server

--- src/actions.js
+++ src/actions.js
@@ -1,19 +1,28 @@
+// action to fetch 25 commits
 function fetchCommits() {
   var oReq2 = new XMLHttpRequest();
-  oReq.open("GET", "http://localhost:8080/commits");
-  oReq.send();
+  oReq2.open("GET", "http://localhost:8080/commits");
+  oReq2.send();
 }
 
-var fetchCommit = function(sha) {
+// action to get one commit base on sha number
+var fetchCommit = function(sha, callback) {
   var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("demo").innerHTML =
-            this.responseText;
+          try {
+            var data = JSON.parse(xhttp.responseText);
+          }
+          catch(err) {
+            console.log(err.message + " in " + xhttp.responseText);
+            return;
+          }
+          callback(data);
        }
     };
   xhttp.open("GET", `http://localhost:8080/commits/${sha}`);
   xhttp.send();
 }
+
 
 module.exports = fetchCommit;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
 import './styles/styles.scss';
 //import { fetchCommit } from "./actions.js";
-// var fetchCommit = require('./actions.js');
+ var fetchCommit = require('./actions.js');
 // Rock some code here
 document.addEventListener("DOMContentLoaded", function(event) {
-    alert('Hi');
+
+  //s how info for each commit
+  fetchCommit('6e0009e18d6ed2580a7f571e3253b63404948804', function(data) {
+
+    var message = data["commit"]["message"].length > 50 ? data["commit"]["message"].substr(0, 50) + "...": data["commit"]["message"];
+
+    // add avatar??
+    var html = "<h3><strong> Author's name </strong>" + data["commit"]["author"]["name"] + "</h3>";
+    html += "<p><strong> Commit time </strong>" + data["commit"]["author"]["date"] + "</p>";
+    html += "<p><strong> commit message: </strong>" + message + "</p>";
+    html += "<p><strong> Additions: </strong>" + data["stats"]["additions"] + "</p>";
+    html += "<p><strong> Deletions: </strong>" + data["stats"]["deletions"] + "</p>";
+    html += "<p><strong> Files: " + data["files"][0]["filename"]+ "</strong></p>";
+    html += "<ul>";
+      // need to add link to the patch of each file 
+       for (var i=0; i < data["files"].length; i++) {
+           html += '<li><a href="' + data["files"][i]["filename"] + '">' + data["files"][i]["filename"] + "</a></li>";
+       }
+    html += "</ul>";
+
+    document.getElementById("root").innerHTML = html;
   });
+
+});
 
 
 
