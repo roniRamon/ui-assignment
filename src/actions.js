6c8c56966c7b5c6537247c904f156d352c309647
RoniRamon <roniramon260@gmail.com>
2018-07-04 13:31:08 -0700

    add link to add 25 more commits

--- src/actions.js
+++ src/actions.js
@@ -1,11 +1,27 @@
 // action to fetch 25 commits
-function fetchCommits() {
-  var oReq2 = new XMLHttpRequest();
-  oReq2.open("GET", "http://localhost:8080/commits");
-  oReq2.send();
-}
 
-// action to get one commit base on sha number
+function fetchCommits(callback) {
+  var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          try {
+            var data = JSON.parse(xhttp.responseText);
+          }
+          catch(err) {
+            console.log(err.message + " in " + xhttp.responseText);
+            return;
+          }
+          callback(data);
+       }
+    };
+  var idx = fetchCommits.calledTimes;
+  xhttp.open("GET", `http://localhost:8080/commits?start=${idx}&limit=25`);
+  xhttp.send();
+  fetchCommits.calledTimes += 25;
+}
+fetchCommits.calledTimes = 0;
+
+// action to get a commit base on sha number
 var fetchCommit = function(sha, callback) {
   var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
@@ -24,5 +40,36 @@ var fetchCommit = function(sha, callback) {
   xhttp.send();
 }
 
+//action to edit a commit message
 
-module.exports = fetchCommit;
+// show commit info
+var commitInfo = function(commit){
+  let html = "";
+
+  var message = commit["commit"]["message"].length > 50 ? commit["commit"]["message"].substr(0, 50) + "...": commit["commit"]["message"];
+  // add avatar??
+
+  html += "<h3><strong> Author's name </strong>" + commit["commit"]["author"]["name"] + "</h3>";
+  html += "<p><strong> Commit time </strong>" + commit["commit"]["author"]["date"] + "</p>";
+  html += "<p><strong> commit message: </strong><a href='" + commit["commit"]["message"] + "'>" + message + "</a></p>";
+  html += "<p><strong> Additions: </strong>" + commit["stats"]["additions"] + "</p>";
+  html += "<p><strong> Deletions: </strong>" + commit["stats"]["deletions"] + "</p>";
+  html += "<p><strong> Files: </strong></p>";
+  html += "<ul>";
+    // need to add link to the patch of each file
+     for (var i=0; i < commit["files"].length; i++) {
+         html += '<li><a href="' + commit["files"][i]["filename"] + '">' + commit["files"][i]["filename"] + "</a></li>";
+     }
+  html += "</ul><hr>";
+
+  var commits = document.createElement("div");
+  commits.innerHTML = html;
+  return commits;
+};
+
+
+module.exports = {
+  fetchCommit,
+  fetchCommits,
+  commitInfo
+};
--- src/index.js
+++ src/index.js
@@ -1,30 +1,31 @@
 import './styles/styles.scss';
-//import { fetchCommit } from "./actions.js";
- var fetchCommit = require('./actions.js');
+var Actions = require('./actions.js');
+var fetchCommit = Actions.fetchCommit;
+var fetchCommits = Actions.fetchCommits;
+var commitInfo = Actions.commitInfo;
 // Rock some code here
 document.addEventListener("DOMContentLoaded", function(event) {
 
-  //s how info for each commit
-  fetchCommit('6e0009e18d6ed2580a7f571e3253b63404948804', function(data) {
-
-    var message = data["commit"]["message"].length > 50 ? data["commit"]["message"].substr(0, 50) + "...": data["commit"]["message"];
-
-    // add avatar??
-    var html = "<h3><strong> Author's name </strong>" + data["commit"]["author"]["name"] + "</h3>";
-    html += "<p><strong> Commit time </strong>" + data["commit"]["author"]["date"] + "</p>";
-    html += "<p><strong> commit message: </strong>" + message + "</p>";
-    html += "<p><strong> Additions: </strong>" + data["stats"]["additions"] + "</p>";
-    html += "<p><strong> Deletions: </strong>" + data["stats"]["deletions"] + "</p>";
-    html += "<p><strong> Files: " + data["files"][0]["filename"]+ "</strong></p>";
-    html += "<ul>";
-      // need to add link to the patch of each file 
-       for (var i=0; i < data["files"].length; i++) {
-           html += '<li><a href="' + data["files"][i]["filename"] + '">' + data["files"][i]["filename"] + "</a></li>";
-       }
-    html += "</ul>";
-
-    document.getElementById("root").innerHTML = html;
+  fetchCommits(function(data) {
+    for (var j=0; j < data.length; j++) {
+      document.getElementById("root").appendChild( commitInfo(data[j]) );
+    }
   });
+
+  // a tag for 25 more commits
+  // if(idxStart < 200) {
+    var moreCommits = document.createElement("a");
+    moreCommits.innerHTML = "Show More Commits";
+    moreCommits.onclick = function(e){
+      fetchCommits(function(data) {
+        for (var j=0; j < data.length; j++) {
+          document.getElementById("root").appendChild( commitInfo(data[j]) );
+        }
+      });
+
+    }
+    document.getElementById("link").appendChild(moreCommits);
+  // }
 
 });
 
@@ -34,3 +35,5 @@ document.addEventListener("DOMContentLoaded", function(event) {
 if (module.hot) {
   module.hot.accept();
 }
+
+//ex fetchCommit('6e0009e18d6ed2580a7f571e3253b63404948804', function(data) {
