a9008c1bbd78b07c4f36d845a51a0bb60e6f31b4
RoniRamon <roniramon260@gmail.com>
2018-07-03 18:57:07 -0700

    webpack config

--- src/actions.js
+++ src/actions.js
@@ -4,8 +4,16 @@ function fetchCommits() {
   oReq.send();
 }
 
-function fetchCommit(sha) {
-  var oReq2 = new XMLHttpRequest();
-  oReq.open("GET", `http://localhost:8080/commits/${sha}`);
-  oReq.send();
+var fetchCommit = function(sha) {
+  var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("demo").innerHTML =
+            this.responseText;
+       }
+    };
+  xhttp.open("GET", `http://localhost:8080/commits/${sha}`);
+  xhttp.send();
 }
+
+module.exports = fetchCommit;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import './styles/styles.scss';
-
+//import { fetchCommit } from "./actions.js";
+// var fetchCommit = require('./actions.js');
 // Rock some code here
-
+document.addEventListener("DOMContentLoaded", function(event) {
+    alert('Hi');
+  });
 
 
 
